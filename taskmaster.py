8494f8b63425afc5c08af065e38a90cf775d4891
Nard Théo <58373303+PandeoF1@users.noreply.github.com>
2024-03-05 15:30:04 +0000

    fix: Default GUI

--- taskmaster.py
+++ taskmaster.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
+import curses
 import sys
 import signal
 import time
 from utils.logger import logger
+from utils.gui import gui
 
-main_log = logger("my_app")
+log = logger("taskmaster")
 
 #Handle ctrl c
 def signal_handler(sig, frame):
-    main_log.log("This is an informational message.")
-    main_log.log("This is a warning message.", level="WARNING")
-    main_log.close()
+    log.log("CTRL+C detected. Exiting...", level="WARNING")
+    log.close()
     sys.exit(0)
 
-if __name__ == "__main__":
+def init_signal():
+    log.log("Initializing signal handler.")
     signal.signal(signal.SIGINT, signal_handler)
-    time.sleep(10)
+
+if __name__ == "__main__":
+    try:
+        log.log("Starting taskmaster.")
+        init_signal()
+        interface = gui()
+        interface.default()
+        while True:
+            log.log(interface.win_active)
+            if interface.win_active == 'default' and interface.default_nav(interface.win['default'].getch()) == -1:
+                break
+            if interface.win_active == 'services' and interface.services_nav(interface.win['services'].getch()) == -1:
+                break
+        interface.end()
+    except Exception as e:
+        log.log(f"An error occurred: {e}", level="ERROR")
+        log.close()
+        sys.exit(1)
---
+++ utils/gui.py
@@ -0,0 +1,141 @@
+import curses
+from utils.logger import logger
+import sys
+log = logger("taskmaster_gui")
+
+class gui:
+    """
+    A class that creates a GUI for the taskmaster application.
+    """
+    
+    pages = [
+        "services",
+        "wip"
+    ]
+    
+    def __init__(self):
+        # Initialize the screen
+        log.log("Initializing screen.")
+        try:
+            self.stdscr = curses.initscr()
+            self.stdscr.keypad(True)
+            self.stdscr.clear()
+            curses.noecho()
+            curses.cbreak()
+            curses.curs_set(0)
+            self.height, self.width = self.stdscr.getmaxyx()
+            log.log(f"Screen size: {self.height}x{self.width}")
+            if self.height < 20 or self.width < 80:
+                log.log("Screen is too small.", level="ERROR")
+                self.end()
+                sys.exit(1)
+            curses.start_color()
+            curses.use_default_colors()
+            self.win = dict()
+            self.win_data = dict()
+            self.default()
+        except:
+            log.log("Failed to initialize screen.", level="ERROR")
+            self.end()
+
+    def box(self, windows):
+        # Create a window with a box around it
+        try:
+            log.log("Loading box.")
+            self.win[windows].box(0, 0)
+            self.win[windows].addstr(0, 2, ' ' + windows + ' ')
+            self.win[windows].refresh()
+        except:
+            log.log("Failed to load box.", level="ERROR")
+            self.end()
+
+    def default(self):
+        # Default page
+        try:
+            log.log("Loading default page.")
+            if 'default' not in self.win:
+                self.win['default'] = curses.newwin(self.height, self.width, 0, 0)
+                self.win_data['default'] = dict()
+                self.win_data['default']['selected'] = self.pages[0]
+            self.win_active = 'default'
+            self.box('default')
+            self.win['default'].addstr(3, 4, "Taskmaster - Main menu")
+            for i, page in enumerate(self.pages):
+                if page == self.win_data['default']['selected']:
+                    self.win['default'].addstr(5 + i, 6, f"{i + 1}. {page.capitalize()}", curses.A_REVERSE)
+                else:
+                    self.win['default'].addstr(5 + i, 6, f"{i + 1}. {page.capitalize()}")
+            self.win['default'].addstr(self.height - 3, 4, "Press 'q' to quit. - (↑•↓ to navigate)")
+            self.win['default'].refresh()
+        except:
+            log.log("Failed to load default page.", level="ERROR")
+            self.end()
+
+    def default_nav(self, key):
+        # Navigate through the pages
+        try:
+            log.log(f"[Default] Key pressed: {key}")
+            if key == 113:  # q
+                return -1
+            elif key == 66:  # ↓
+                # select next in the list
+                index = self.pages.index(self.win_data['default']['selected'])
+                if index < len(self.pages) - 1:
+                    index += 1
+                    self.win_data['default']['selected'] = self.pages[index]
+            elif key == 65:  # ↑
+                # select previous in the list
+                index = self.pages.index(self.win_data['default']['selected'])
+                if index > 0:
+                    index -= 1
+                    self.win_data['default']['selected'] = self.pages[index]
+            elif key == 10: # Enter
+                # Open the selected page
+                if self.win_data['default']['selected'] == "services":
+                    self.services()
+                return
+            self.default()
+        except:
+            log.log("[Default] Failed to navigate.", level="ERROR")
+            self.end()
+
+    def services_nav(self, key):
+        try:
+            log.log(f"[Services] Key pressed: {key}")
+            if key == 113:  # q
+                self.default()
+                return
+            self.services()
+        except:
+            log.log("[Services] Failed to navigate.", level="ERROR")
+            self.end()
+
+    def services(self):
+        # Services page
+        try:
+            log.log("Loading services page.")
+            if 'services' not in self.win:
+                self.win['services'] = curses.newwin(self.height, self.width, 0, 0)
+                self.win_data['services'] = dict()
+                self.win_data['services']['selected'] = "service1"
+            self.win_active = 'services'
+            self.box('services')
+            self.win['services'].addstr(3, 4, "Taskmaster - Services")
+            self.win['services'].addstr(self.height - 3, 4, "Press 'q' to go back. - (↑•↓ to navigate)")
+            self.win['services'].refresh()
+        except:
+            log.log("Failed to load services page.", level="ERROR")
+            self.end()
+
+    def end(self):
+        # Restore terminal settings and end curses mode
+        try:
+            self.stdscr.keypad(False)
+            curses.nocbreak()
+            curses.echo()
+            curses.endwin()
+        except:
+            log.log("Failed to end screen.", level="ERROR")
+
+    def __del__(self):
+        self.end()

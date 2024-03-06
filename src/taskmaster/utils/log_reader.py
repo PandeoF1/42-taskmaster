f5057c0d799c8574f0b3dcfad0e0c0f00b945c86
Timothée Lafay <87896378+Tlafay1@users.noreply.github.com>
2024-03-06 17:03:53 +0000

    oops forgot that

---
+++ src/taskmaster/utils/log_reader.py
@@ -0,0 +1,144 @@
+from .logger import LOG_FILE
+
+
+class LogReader:
+    """
+    LogReader class for reading log files and managing the buffer.
+
+    Args:
+        log_file (str): The path to the log file. Default is LOG_FILE.
+        log_level (str): The log level. Default is "DEBUG".
+        size (int): The size of the buffer. Default is 20.
+
+    Attributes:
+        size (int): The size of the buffer.
+        end (bool): Whether the end of the file has been reached.
+
+    Methods:
+        up: Moves the buffer up by one line.
+        down: Moves the buffer down by one line.
+        lines: Returns the lines in the buffer.
+        latest: Places the start of the buffer at the end of the file.
+    """
+
+    def __init__(
+        self,
+        log_file: str = LOG_FILE,
+        log_level: str = "DEBUG",
+        size: int = 20,
+    ):
+        self._log_file = open(log_file, "r")
+        self._log_level = log_level
+        self._start = 0
+        self._size = size
+        assert self._size > 0
+        self._buffer = []
+
+    def __del__(self):
+        self._log_file.close()
+
+    def _read(self) -> list[str]:
+        """
+        Reads the file and appends new lines to the buffer.
+
+        Returns:
+            list[str]: The lines read from the file.
+
+        """
+        new_lines = self._log_file.readlines()
+        self._buffer.extend(new_lines)
+
+        end = (
+            self._start + self._size
+            if self._start + self._size < len(self._buffer)
+            else len(self._buffer)
+        )
+
+        return self._buffer[self._start : end]
+
+    @property
+    def size(self) -> int:
+        """
+        Getter for the size attribute.
+
+        Returns:
+            int: The size of the buffer.
+
+        """
+        return self._size
+
+    @size.setter
+    def size(self, size: int) -> None:
+        """
+        Setter for the size attribute.
+
+        Args:
+            size (int): The new size of the buffer.
+
+        Returns:
+            None
+
+        """
+        self._start -= size - self._size
+        self._size = size
+        return self._size
+
+    def up(self) -> int:
+        """
+        Moves the buffer up by one line.
+
+        Returns:
+            int: The new starting index of the buffer.
+
+        """
+        self._read()
+        if self._start + self._size < len(self._buffer):
+            self._start += 1
+        return self._start
+
+    def down(self) -> int:
+        """
+        Moves the buffer down by one line.
+
+        Returns:
+            int: The new starting index of the buffer.
+
+        """
+        self._read()
+        if self._start > 0:
+            self._start -= 1
+        return self._start
+
+    @property
+    def end(self) -> bool:
+        """
+        Returns whether the end of the file has been reached or not.
+
+        Returns:
+            bool: True if the end of the file has been reached, False otherwise.
+
+        """
+        self._read()
+        return self._start + self._size >= len(self._buffer)
+
+    @property
+    def lines(self) -> list[str]:
+        """
+        Returns the lines in the buffer.
+
+        Returns:
+            list[str]: The lines in the buffer.
+
+        """
+        return self._read()
+
+    def latest(self) -> None:
+        """
+        Places the start of the buffer at the end of the file.
+
+        Returns:
+            None
+
+        """
+        self._read()
+        self._start = len(self._buffer) - self._size

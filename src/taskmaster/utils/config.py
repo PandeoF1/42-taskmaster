2d5517d270a3181c6be91d64f78ddd48d7e17796
Nard Théo <58373303+PandeoF1@users.noreply.github.com>
2024-03-07 09:43:41 +0000

    fix: New management of configuration file and parsing

--- src/taskmaster/utils/config.py
+++ src/taskmaster/utils/config.py
@@ -1,42 +1,96 @@
 import yaml
 from .logger import logger
+from cerberus import Validator, SchemaError
 
 
 def validate_dict(data: dict, template: dict) -> bool:
     for key, expected_type in template.items():
         if not len(data) == len(template):
-            return 'Invalid number of keys.'
+            return "Invalid number of keys."
         if key not in data:
-            return key + ' is missing.'
+            return key + " is missing."
         if not isinstance(data[key], expected_type):
-            return key + ' is not of type ' + str(expected_type)
+            return key + " is not of type " + str(expected_type)
     return None
 
 
-_taskmaster_template = dict()
-
-
-_taskmaster_template['main'] = {
-    "services": list
+schema = {
+    "services": {
+        "type": "list",
+        "required": True,
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "name": {
+                    "type": "string",
+                    "required": True,
+                },
+                "cmd": {
+                    "type": "string",
+                    "required": True,
+                },
+                "numprocs": {
+                    "type": "integer",
+                    "min": 1,
+                    "max": 32,
+                    "required": True,
+                },
+                "umask": {
+                    "type": "integer",
+                    "min": 0,
+                    "max": 777,
+                    "required": True,
+                },
+                "workingdir": {
+                    "type": "string",
+                    "required": True,
+                },
+                "autostart": {
+                    "type": "boolean",
+                    "required": True,
+                },
+                "autorestart": {
+                    "type": "string",
+                    "required": True,
+                    "allowed": ["unexpected", "always", "never"],
+                },
+                "exitcodes": {
+                    "type": "list",
+                    "required": True,
+                    "schema": {"type": "integer", "min": 0, "max": 255},
+                },
+                "startretries": {
+                    "type": "integer",
+                    "min": 0,
+                    "max": 10,
+                    "required": True,
+                },
+                "starttime": {"type": "integer", "min": 0, "required": True},
+                "stopsignal": {
+                    "type": "string",
+                    "required": True,
+                    "allowed": ["TERM", "HUP", "INT", "QUIT", "KILL", "USR1", "USR2"],
+                },
+                "stoptime": {
+                    "type": "integer",
+                    "min": 0,
+                    "required": True,
+                },
+                "stdout": {
+                    "type": "string",
+                },
+                "stderr": {
+                    "type": "string",
+                },
+                "user": {
+                    "type": "string",
+                },
+            },
+        },
+    }
 }
 
-_taskmaster_template['service'] = {
-    "name": str,
-    "command": str,
-    "replicas": int,
-    "autostart": bool,
-    "restart": str,  # "always", "never", "unexpected"
-    "max_restart": int,
-    "unexpected_exit_code": list, 
-    "started_at": int,
-    "force_stop_after": int,
-    "stop_signal": str,  # "SIGTERM", "SIGKILL"
-    "stop_timeout": int,  # Time before force kill
-    "logs": list, # "stdout", "stderr" # If not present they are not logged
-    "env": list,
-    "working_dir": str,
-    "umask": str
-}
+validator = Validator(schema)
 
 
 class Config:
@@ -49,27 +103,23 @@ class Config:
         try:
             with open("taskmaster.yml", "r") as file:
                 content = yaml.safe_load(file)
-                validator = validate_dict(content, _taskmaster_template['main'])
-                if validator is not None:
-                    logger.error(f"Invalid configuration file. {validator}")
-                    raise Exception(validator)
-                self.services = content['services']
-                for service in self.services:
-                    validator = validate_dict(service, _taskmaster_template['service'])
-                    if validator is not None:
-                        logger.error(f"Invalid configuration file. {validator}")
-                        raise Exception(validator)
+                if not validator.validate(content):
+                    raise SchemaError(validator.errors)
                 self.config = content
         except FileNotFoundError:
             # Throw an error if the file does not exist
             logger.error("No configuration file found.")
             raise FileNotFoundError("No configuration file found.")
+        except SchemaError as e:
+            print(f"Invalid configuration file: {e}")
+            logger.error(f"Invalid configuration file. {e}")
+            raise Exception(f"Invalid configuration file.")
         except Exception:
             logger.error(f"Failed to read configuration file.")
             raise Exception(f"Failed to read configuration file.")
 
     def get_services(self):
-        return self.config['services']
+        return self.config["services"]
 
     def get_services_keys(self):
-        return self.config['services']
+        return self.config["services"]

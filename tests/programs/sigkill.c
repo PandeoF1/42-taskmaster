e58eb9dafe24d02db88f731e657219714ca32384
Nard Théo <58373303+PandeoF1@users.noreply.github.com>
2024-03-15 12:29:36 +0000

    fix: edit sigkill

--- tests/programs/sigkill.c
+++ tests/programs/sigkill.c
@@ -4,6 +4,8 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void sigusr1_handler(int signo) {
 	printf("Received SIGUSR1 signal!\n");
@@ -15,8 +17,9 @@ int main(int argc, char **argv)
 	printf("---- sigkill test ----\n");
 	signal(SIGUSR1, sigusr1_handler);
 	while (1) {
-		printf("Waiting for SIGUSR1 signal...\n");
+		printf("Waiting for SIGUSR1 signal... %d\n", getpid());
 		sleep(1);
+		fflush(stdout);
 	}
 	return 0;
 }
